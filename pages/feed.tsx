21bec4537049220950daba02453d8e77fb90de79
jitka.maresova <jitka.maresova@dnai.ai>
2022-06-01 12:49:41 +0200

    feed responsive

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -3,14 +3,15 @@ import type { NextPage } from "next";
 import FeedCard from "../components/feed/FeedCard";
 import { feeds } from "../public/data/feeds";
 
-
 const Feed: NextPage = (): ReactElement => {
   return (
     <div className="main">
       <h1 className="title">Co krmit:</h1>
-      {feeds.map((feed) => (
-        <FeedCard key={feed.id} feed={feed} />
-      ))}
+      <div className="feeds">
+        {feeds.map((feed) => (
+          <FeedCard key={feed.id} feed={feed} />
+        ))}
+      </div>
     </div>
   );
 };
